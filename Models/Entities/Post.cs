2014858c21c33f88aa59f8087affe08f11078aa7
Karol Komorowski <komor242@gmail.com>
2018-12-14 18:34:36 +0100

    Add user_id to Post entity

--- Models/Entities/Post.cs
+++ Models/Entities/Post.cs
@@ -10,5 +10,7 @@ namespace TestGit1.Models.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+        public int User_id { get; set; }
+
     }
 }
