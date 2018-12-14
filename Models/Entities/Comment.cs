9b7a848bb9a74db8725bae15c36731fa1f733af3
Karol Komorowski <komor242@gmail.com>
2018-12-14 18:43:02 +0100

    Add Comment entity

---
+++ Models/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGit1.Models.Entities
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public int User_Id { get; set; }
+
+        public string Date { get; set; }
+    }
+}
