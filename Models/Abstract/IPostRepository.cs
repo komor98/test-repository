7a89be36cef8bbfdab94914dc54bec4e42010293
Karol Komorowski <komor242@gmail.com>
2018-12-14 13:26:26 +0100

    Add Post Repository

---
+++ Models/Abstract/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGit1.Models.Abstract
+{
+    public class IPostRepository
+    {
+    }
+}
