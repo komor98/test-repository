aabe36dd790d202bb58450c8f56dbe74001f471e
Karol Komorowski <komor242@gmail.com>
2018-12-14 19:31:37 +0100

    Add properties to User Entity

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -7,5 +7,10 @@ namespace TestGit1.Models.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public byte[] Image { get; set; }
     }
 }
